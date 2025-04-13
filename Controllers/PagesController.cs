771ada40b52b5d31393b81d41f4b8908729d7288
inhellcorruption <inhellcorruption@yahoo.com>
2025-04-13 12:19:11 +0700

    Commit Awal

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexa.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult Error500()
+        {
+            return View();
+        }
+        public IActionResult Directory()
+        {
+            return View();
+        }
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+        public IActionResult Lock()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Recoverpw()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Timeline()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lexa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
