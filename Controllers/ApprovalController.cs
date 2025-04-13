82a20b3e2fc19d00fd1e4e79aa0e552fe1a7dfed
akitzuke <graceismyexcontinent@gmail.com>
2025-04-13 12:46:28 +0700

    Merge Kerjaan Teguh

---
+++ Controllers/ApprovalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexa.Controllers
+{
+    public class ApprovalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StockGudangController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexa.Controllers
+{
+    public class StockGudangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TambahBarang()
+        {
+            return View();
+        }
+    }
+}
